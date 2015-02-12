3b4e7a539d9a892afa8a5428ca57d133bdd21334
Dustin Kraft <kraftdr@gmail.com>
2015-02-12 12:47:46 -0700

    checkin

---
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+
+    // When a new deck is created, you’ll create a card of each rank for each suit and add them to the deck of cards, 
+    //      which in this case will be a List of Card objects.
+    //
+    // A deck can perform the following actions:
+	//     void Shuffle() -- Merges the discarded pile with the deck and shuffles the cards
+	//     List<card> Deal(int numberOfCards) - returns a number of cards from the top of the deck
+	//     void Discard(Card card) / void Discard(List<Card> cards) - returns a card from a player to the 
+	//         discard pile	
+    // 
+    // A deck knows the following information about itself:
+	//     int CardsRemaining -- number of cards left in the deck
+	//     List<Card> DeckOfCards -- card waiting to be dealt
+    //     List<Card> DiscardedCards -- cards that have been played
+    class Deck
+    {
+
+    }
+
+    
+    // What makes a card?
+	//     A card is comprised of it’s suit and its rank.  Both of which are enumerations.
+    class Card
+    {
+
+    }
+}
---
+++ DeckOfCards/DeckOfCards/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DeckOfCards
+{
+    [TestFixture]
+    class Test
+    {
+        
+        [Test]
+        public void ConstructorTest()
+        {
+            Deck deck = new Deck();
+            Assert.IsTrue(deck.DeckOfCards.Distinct().Count() == 52, "Deck does not have 52 unique cards");
+        }
+
+        
+        [Test]
+        public void DealTest()
+        {
+            Deck deck = new Deck();
+            List<Card> hand = deck.Deal(5);
+            Assert.IsTrue(deck.DeckOfCards.Count == 47, "DeckOfCards should have 47 cards after dealing 5");
+        }
+
+        
+        [Test]
+        public void DiscardTest()
+        {
+            Deck deck = new Deck();
+            List<Card> hand = deck.Deal(5);
+            deck.Discard(hand);
+            Assert.IsTrue(deck.DiscardedCards.Count == 5, "Discard pile should have 5 cards after discarding 5 from a hand");
+        }
+
+        [Test]
+        public void ShuffleTest()
+        {
+            Deck deck = new Deck();
+            Card card = deck.DeckOfCards[10];
+            deck.Shuffle();
+            Assert.IsTrue(deck.DeckOfCards[10] != card, "Card was still at the same index.");
+        }
+
+    }
+}
